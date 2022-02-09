1172eeb694f7ef049be269b4aba74cb2ff686457
pgoncalves2002 <p.goncalves2002@gmail.com>
2022-02-08 22:47:37 -0300

    inicio do codigo em si

--- sketch.js
+++ sketch.js
@@ -1,7 +1,27 @@
+let imagemDaEstrada;
+let imagemDoAtor;
+let imagemCarro;
+
+//variaveis do carro
+let xCarro = 420;
+let yCarro = 40;
+
+function preload(){
+  imagemDaEstrada = loadImage("imagens/estrada.png");
+  imagemDoAtor = loadImage("imagens/ator-1.png");
+  imagemCarro = loadImage("imagens/carro-1.png")
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 400);
 }
 
 function draw() {
-  background(0);
+  background(imagemDaEstrada);
+  mostraAtor()
+  image(imagemCarro, xCarro, yCarro, 50, 40)
+}
+
+function mostraAtor(){
+  image(imagemDoAtor, 100, 366, 30, 30)
 }
