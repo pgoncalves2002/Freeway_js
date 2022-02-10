f261f1125e8b3da6ca57843caa722bbde6216216
pgoncalves2002 <p.goncalves2002@gmail.com>
2022-02-10 11:44:22 -0300

    add alguns refinamentos e criacao de colisao

--- ator.js
+++ ator.js
@@ -1,6 +1,8 @@
 //ator
 let yAtor = 366;
-let xAtor = 100;
+let xAtor = 85;
+let colisao = false;
+let meusPontos = 0;
 
 function mostraAtor(){
     image(imagemDoAtor, xAtor, yAtor, 30, 30);
@@ -11,12 +13,50 @@ function mostraAtor(){
       yAtor -= 3;
     }
     if (keyIsDown(DOWN_ARROW)){
+      if (podeSeMoverY()){
       yAtor += 3;
     }
+  }
     if (keyIsDown(LEFT_ARROW)){
-      xAtor -= 3;
+        xAtor -= 3;
     }
+  
     if (keyIsDown(RIGHT_ARROW)){
-      xAtor += 3;
+        xAtor += 3;
+      }
+  }
+  
+
+  function verificaColisao(){
+    for (let i = 0; i < imagemCarros.length; i++){
+      colisao = collideRectCircle(xCarros[i], yCarros[i], comprimentoCarro, alturaCarro, xAtor, yAtor, 15)
+        if(colisao){
+          voltaAtorParaPosicaoInicial();
+          if (meusPontos > 0){
+          meusPontos -= 1;
+          }
+        }
     }
   }
+  function marcaPonto(){
+    if (yAtor < 15){
+      meusPontos += 1;
+      voltaAtorParaPosicaoInicial();
+    }
+  }
+
+  function incluiPontos(){
+    textAlign(CENTER);
+    textSize(25);
+    fill(color(255, 240, 60))
+    text(meusPontos, width / 5, 27);
+  }
+ function voltaAtorParaPosicaoInicial(){
+   yAtor = 366;
+   xAtor = 85;
+ }
+
+ function podeSeMoverY(){
+   return yAtor < 366;
+ }
+
--- carro.js
+++ carro.js
@@ -1,22 +1,23 @@
 // codigo do carro
-let yCarros = [40, 96, 150]
-let xCarros = [600, 600,600]
-let velocidadeCarros = [2, 2.5, 3.2];
-
+let yCarros = [40, 96, 150, 210, 270, 318]
+let xCarros = [600, 600,600, 600, 600, 600]
+let velocidadeCarros = [2, 2.5, 3.2, 5, 3.3, 2.3];
+let comprimentoCarro = 50;
+let alturaCarro = 40;
 function mostraCarro(){
-    for(let i = 0; i < imagemCarros.length; i = i + 1){
-    image(imagemCarros[i], xCarros[i], yCarros[i], 50, 40);
+    for(let i = 0; i < imagemCarros.length; i++){
+    image(imagemCarros[i], xCarros[i], yCarros[i], comprimentoCarro, alturaCarro);
     }
   }
   
 function movimentaCarro(){
-    for (let i = 0; i < imagemCarros.length; i = i + 1){
+    for (let i = 0; i < imagemCarros.length; i++){
    xCarros[i] -= velocidadeCarros[i];
     }
   }
  
   function voltaPosicaoInicialDoCarro(){
-      for (let i = 0; i < imagemCarros.length; i = i + 1){
+      for (let i = 0; i < imagemCarros.length; i++){
     if (passouTodaATela(xCarros[i])){
         xCarros[i] = 600;
         }
